058daa1d8dcb98270fd82800a6464e3d236ab52f
Rodriguez Ariza <endo.rodriguez@externos-co.cencosud.com>
2025-01-15 16:20:15 -0500

    Finalizacion de la platilla de la factura

--- src/main/java/com/andres/springboot/springmvc/app/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.andres.springboot.springmvc.app.repositories;
-
-import com.andres.springboot.springmvc.app.entities.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Customer, Long> {
-
-}
