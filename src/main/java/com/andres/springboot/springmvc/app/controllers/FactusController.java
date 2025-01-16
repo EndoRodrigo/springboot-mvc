d44cc82510d5118cff1ea85ba13f33fae4a0a8f9
EndoRodrigoRodriguez <endorodrigo19@gmail.com>
2025-01-16 15:04:45 -0500

    feat: fix: arreglo de la platilla form

--- src/main/java/com/andres/springboot/springmvc/app/controllers/FactusController.java
+++ src/main/java/com/andres/springboot/springmvc/app/controllers/FactusController.java
@@ -1,5 +1,6 @@
 package com.andres.springboot.springmvc.app.controllers;
 
+import com.andres.springboot.springmvc.app.entities.Items;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +16,9 @@ import com.andres.springboot.springmvc.app.entities.Factus;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/factus")
@@ -32,15 +36,20 @@ public class FactusController {
     @GetMapping("/form")
     public String listE(Model model) {
         model.addAttribute("title", "Listado de facturas generadas");
-        model.addAttribute("factu", new Factus());
+        Customer customer = new Customer();
+        Factus factus = new Factus();
+        List<Items> iten = new ArrayList<>();
+        factus.setCustomer(customer);
+        factus.setItems(iten);
+        model.addAttribute("factu", factus);
         return "form";
     }
 
 
     @PostMapping("/form")
-    public String form(@Valid Customer user, BindingResult result, Model model, RedirectAttributes redirect, SessionStatus status) {
+    public String form(@Valid Factus factu, BindingResult result, Model model, RedirectAttributes redirect, SessionStatus status) {
 
-        log.info(user.toString());
+        log.info(factu.toString());
         return "form";
     }
 
